127e8560bf83387132030e32d79186ff831a7d0d
Zachary Carlin <sock@Zacharys-MacBook-Pro.local>
2019-06-01 01:32:07 -0400

    netflix button and input to change ip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="remote">
+   <b-row align-h='center'>
+      <h3>IP:</h3> <input type="number" v-model="ip">
+    </b-row>
     <div id="row1">
       <b-row align-h="center">
+        <b-button variant="outline-info" @click="back()">Back</b-button>
       <b-button variant="outline-info"  @click="home()">Home</b-button>
       </b-row>
       <b-row align-h="center">
@@ -24,6 +28,11 @@
       <b-row align-h="center">
       <b-button variant="outline-info" @click="down()">Down</b-button>
       </b-row>
+      <b-row align-h="center">
+        <b-button variant="danger" @click="netflix()">Netflix</b-button>
+                <b-button @click="getApps()">Apps</b-button>
+
+      </b-row>
     </div> 
   </div>
 </template>
@@ -33,38 +42,61 @@
 import axios from 'axios'
 export default {
   name: 'home',
+  data(){
+    return{
+      ip:17
+    }
+  },
   components: {
     // HelloWorld
   },
   methods:{
+ getApps(){
+      axios.get('http://192.168.0.'+this.ip+':8060/query/apps').then((res)=>{
+        console.log(res)
+      })
+     
+    },
+    netflix(){
+      axios.post('http://192.168.0.'+this.ip+':8060/launch/12')
+    },
+    back(){
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/back')
+    },
     togglePlayPause(){
-      axios.post('http://192.168.0.2:8060/keypress/play')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/play')
     },
     up(){
-      axios.post('http://192.168.0.2:8060/keypress/up')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/up')
     },
     left(){
-      axios.post('http://192.168.0.2:8060/keypress/left')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/left')
     },
     right(){
-      axios.post('http://192.168.0.2:8060/keypress/right')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/right')
     },
     down(){
-      axios.post('http://192.168.0.2:8060/keypress/down')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/down')
     },
     home(){
-      axios.post('http://192.168.0.2:8060/keypress/home')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/home')
     },
     ok(){
-      axios.post('http://192.168.0.2:8060/keypress/select')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/select')
     },
     volumeUp(){
-      axios.post('http://192.168.0.2:8060/keypress/VolumeUp')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/VolumeUp')
     },
     volumeDown(){
-      axios.post('http://192.168.0.2:8060/keypress/VolumeDown')
+      axios.post('http://192.168.0.'+this.ip+':8060/keypress/VolumeDown')
     }
   
   }
 }
 </script>
+<style>
+button{
+  width: 33vw;
+  height: 10vh;
+}
+</style>
